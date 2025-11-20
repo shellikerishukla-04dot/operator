f0b4125a95d7a4d84d5708830139de573874edb0
shellikerishukla-04dot <shellikerishukla@gmail.com>
2025-11-20 16:49:14 +0530

    save

---
+++ scores.py
@@ -0,0 +1,12 @@
+import sys
+if len(sys.argv) > 1:
+    scores = sys.argv[1:]  
+    print("User provided input values:")
+else:
+    print("No command-line input provided. Using default values:")
+    scores = ["10", "20", "30", "40"] 
+total = sum(eval(x) for x in scores)
+average = total / len(scores)
+print("Sum of scores =", total)
+print("Average of scores =", average)
+
